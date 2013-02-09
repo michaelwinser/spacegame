3eacc348701989378d8930d24c1f38ae55f6a2e6
Jonah Boughton <jonahb360@gmail.com>
2013-02-09 15:51:25 -0500

    Circles (moving bodies)

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,38 @@
 function love.draw()
 	love.graphics.print("SPACEGAME", 400, 400)
+	for i,body in ipairs(bodies) do
+		love.graphics.circle("line", body.x, body.y, 10)
+	end
+end
+
+function love.update()
+	for i,body in ipairs(bodies) do 
+		updateBody(body)
+	end
+end
+
+function love.load()
+	bodies = {}
+	for i=1,10 do
+		bodies[i]=createBody(math.random(love.graphics.getWidth()), 
+			math.random(love.graphics.getHeight()), 
+			math.random(11)-6, math.random(11)-6,
+		0, 0)
+	end
+end
+
+function createBody(x, y, dx, dy, bodyType, size)
+	body = {}
+	body.x = x
+	body.y = y
+	body.dx = dx
+	body.dy = dy
+	body.bodyType = bodyType
+	body.size = size
+	return body
+end
+
+function updateBody(body)
+	body.x = body.x + body.dx
+	body.y = body.y + body.dy
 end
