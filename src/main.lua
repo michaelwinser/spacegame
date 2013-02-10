c775b13dc418d86c2054a5b9adb39dcab1cd83a0
Michael Winser <michaelw.net@gmail.com>
2013-02-09 22:26:12 -0500

    Changed ship physics and controls
    The ship now rotates (pretend its on gyroscopes) and thrusts.  Movement
    is now oriented based on rotation.

--- src/main.lua
+++ src/main.lua
@@ -3,30 +3,32 @@ SHIP = 2
 		
 WHITE = { red=255, green=255, blue=255, alpha=255}
 GREEN = { red=0, green=255, blue=0, alpha=255}
+
+debugship = false;
+
 function love.draw()
 	love.graphics.print("SPACEGAME", 400, 400)
+  if debugship then
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print(string.format("x %d y %d angle %f thrust %f dx %f dy %f dangle %f", ship.x, ship.y, ship.angle, ship.thrust, ship.dx, ship.dy, ship.dr), 0, 0)
+  end
+  
 	for i,body in ipairs(bodies) do
-		love.graphics.setColor(body.color.red, body.color.green, body.color.blue, body.color.alpha)
+		if body.image ~= nil then
+			love.graphics.setColor(255, 255, 255, 255)
+			love.graphics.draw(body.image, body.x, body.y, body.angle, 1  , 1, body.image:getWidth() / 2, body.image:getHeight() / 2)
+		end
+    
+    love.graphics.setColor(body.color.red, body.color.green, body.color.blue, body.color.alpha)
 		if body.bodyType == ROCK then
 			love.graphics.circle("fill", body.x, body.y, body.size)
-		else
-			love.graphics.circle("line", body.x, body.y, body.size)
+		elseif (body.bodyType == SHIP) then
+			love.graphics.circle("line", body.x, body.y, body.size, 32)
 		end
 	end
 end
 
-function love.update()
-	
-	if love.keyboard.isDown('w') then
-		ship.dy = ship.dy - 0.1
-	elseif love.keyboard.isDown('a') then
-		ship.dx = ship.dx - 0.1
-	elseif love.keyboard.isDown('s') then
-		ship.dy = ship.dy + 0.1
-	elseif love.keyboard.isDown('d') then
-		ship.dx = ship.dx + 0.1
-	end
-	
+function love.update()	
 	for i,body in ipairs(bodies) do 
 		updateBody(body)
 	end
@@ -51,17 +53,18 @@ end
 function normalloader()
 
 	bodies = {}
-	for i=1,10 do
+  local rockCount = 11
+	for i=1,rockCount do
 		bodies[i] = createRock()
 	end
-	bodies[11] = createShip()
-	ship = bodies[11]
+	ship = bodies[rockCount + 1] = createShip()
+	ship.image = love.graphics.newImage("ship.png");
+	ship.size = ship.image:getWidth() / 2;
 end
 
 function createShip()
 	return createBody(love.graphics.getWidth() / 2, 
-		love.graphics.getHeight() / 2,
-	0, 0, SHIP, 10, {red=255, green=255, blue=255, alpha=127})
+		love.graphics.getHeight() / 2,	0, 0, SHIP, 10, {red=255, green=255, blue=255, alpha=127})
 end
 
 function createRock()
@@ -80,6 +83,9 @@ function createBody(x, y, dx, dy, bodyType, size, color)
 	body.bodyType = bodyType
 	body.size = size
 	body.color = color
+	body.dr = 0;
+	body.angle = 0
+	body.thrust = 0;
 	return body
 end
 
@@ -92,18 +98,22 @@ function createRandom ()
 end
 
 function updateBody(body)
+  body.dx = body.dx + math.cos(body.angle) * body.thrust;
+  body.dy = body.dy + math.sin(body.angle) * body.thrust;
+  
 	body.x = body.x + body.dx
 	body.y = body.y + body.dy
-	if body.x <= 0 then
+	body.angle = body.angle + body.dr;
+	if body.x <= body.size then
 		body.dx = -body.dx
 	end
-	if body.x >= love.graphics.getWidth() then
+	if body.x >= love.graphics.getWidth() - body.size then
 		body.dx = -body.dx
 	end
-		if body.y <= 0 then
+		if body.y <= body.size then
 		body.dy = -body.dy
 	end
-	if body.y >= love.graphics.getHeight() then
+	if body.y >= love.graphics.getHeight() - body.size then
 		body.dy = -body.dy
 	end
 end
@@ -114,6 +124,19 @@ function love.keypressed(key, unicode)
 	elseif key == 'e' then
 		ship.dx = 0
 		ship.dy = 0
+    ship.thrust = 0
+    ship.dr = 0
+    ship.angle = 0
+    ship.x = love.graphics.getWidth() / 2;
+    ship.y = love.graphics.getHeight() / 2;
+  elseif key == '.' then
+    debugship = not debugship
+  elseif key == "up" then
+    ship.thrust = ship.thrust + 0.1
+  elseif key == "right" then
+    ship.dr = math.min(ship.dr + 0.01, 0.02)
+  elseif key == "left" then
+    ship.dr = math.max(ship.dr - 0.01, -0.02)
 	end
 end
 
@@ -124,10 +147,8 @@ function detectCollisions()
 				if hasCollided(body1, body2) then
 					body1.color = {red = math.random(255), green = math.random(255), blue = math.random(255), alpha = body1.color.alpha}
 					body2.color = {red = math.random(255), green = math.random(255), blue = math.random(255), alpha = body2.color.alpha}
-					print(body1.color.red, body1.color.green, body1.color.blue)
-					print(body2.color.red, body2.color.green, body2.color.blue)
-				else
-					print("No collision")
+--					print(body1.color.red, body1.color.green, body1.color.blue)
+--					print(body2.color.red, body2.color.green, body2.color.blue)
 				end
 			end
 		end
@@ -146,7 +167,7 @@ function hasCollided(body1, body2)
 --	print("dy", distanceY)
 --	print("dx2 + dy2", distanceX * distanceX + distanceY * distanceY)
 --	print("collisionDistance2", collisionDistance * collisionDistance)
-	print("collided", collision )
+--	print("collided", collision )
 	return collision;
 end
 	
